7e17948eddfde7f56c4fa30bb65b86f752ef8833
Andrea Azzini <and.azzini@gmail.com>
2015-05-10 03:19:31 +0200

    first commit

---
+++ src/main/java/com/azzarcher/colormanager/ColorManager.java
@@ -0,0 +1,92 @@
+package com.azzarcher.colormanager;
+
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+
+public class ColorManager {
+    private static final int DEFAULT_DELAY = 1000;
+
+    private Window mWindow;
+    private Toolbar mToolbar;
+    private Integer mToolbarColor;
+    private Integer mStatusBarColor;
+
+    public ColorManager setToolbar(Toolbar toolbar) {
+        mToolbar = toolbar;
+        return this;
+    }
+
+    public ColorManager setToolbarColor(int toolbarColor) {
+        // todo controls on the toolbarColor variable validity
+        mToolbar.setBackgroundColor(toolbarColor);
+        mToolbarColor = toolbarColor;
+        return this;
+    }
+
+    public ColorManager setStatusBarColor(Window window, int statusBarColor) {
+        // todo controls on the statusBarColor variable validity
+        window.setStatusBarColor(mStatusBarColor);
+        mWindow = window;
+        mStatusBarColor = statusBarColor;
+        return this;
+    }
+
+    public void animateToolbar(int toColor) throws NoToolbarColorException {
+        // todo controls on the toColor variable validity
+        if (mToolbarColor != null) {
+            ValueAnimator animator = ValueAnimator.ofObject(new ArgbEvaluator(), mToolbarColor, toColor);
+            animator.setDuration(DEFAULT_DELAY);
+            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animator) {
+                    int transitionColor = (int) animator.getAnimatedValue();
+                    mToolbar.setBackgroundColor(transitionColor);
+                    mToolbarColor = transitionColor;
+                }
+            });
+            animator.start();
+        } else throw new NoToolbarColorException();
+    }
+
+    public void animateStatusBar(int toColor) throws NoStatusBarColorException {
+        // todo controls on the toColor variable validity
+        if (mStatusBarColor != null) {
+            ValueAnimator animator = ValueAnimator.ofObject(new ArgbEvaluator(), mStatusBarColor, toColor);
+            animator.setDuration(DEFAULT_DELAY);
+            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animator) {
+                    int transitionColor = (int) animator.getAnimatedValue();
+                    mWindow.setStatusBarColor(transitionColor);
+                    mStatusBarColor = transitionColor;
+                }
+            });
+            animator.start();
+        } else throw new NoStatusBarColorException();
+    }
+
+    public static int generateDarkColorFromPrimary(int primaryColor) {
+        // Darken prominent color
+        float[] hsv = new float[3];
+        Color.colorToHSV(primaryColor, hsv);
+        hsv[2] *= 0.8f; // value component
+        return Color.HSVToColor(hsv);
+    }
+
+    public class NoToolbarColorException extends Exception {
+        @Override
+        public void printStackTrace() {
+            System.err.println("No toolbar color has been set up for this color manager");
+        }
+    }
+
+    public class NoStatusBarColorException extends Exception {
+        @Override
+        public void printStackTrace() {
+            System.err.println("No status bar color has been set up for this color manager");
+        }
+    }
+}
